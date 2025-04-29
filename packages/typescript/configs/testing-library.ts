cc1eea2d2138a924d1ee6cd0dd950d3062a10ae6
William Thorsen <william@thorsen.dev>
2025-04-28 21:55:23 -0700

    ts|feat: Add Jest DOM rules to Testing Library config

--- packages/typescript/configs/testing-library.ts
+++ packages/typescript/configs/testing-library.ts
@@ -1,6 +1,13 @@
+import jestDomPlugin from 'eslint-plugin-jest-dom';
 import testingLibraryPlugin from 'eslint-plugin-testing-library';
+import tseslint from 'typescript-eslint';
 
-export const reactTestingLibrary = testingLibraryPlugin.configs['flat/react'];
+export const reactTestingLibrary = tseslint.config({
+  extends: [
+    jestDomPlugin.configs['flat/all'], //
+    testingLibraryPlugin.configs['flat/react'],
+  ],
+});
 
 // Add other configs as needed.
 const configs = {
