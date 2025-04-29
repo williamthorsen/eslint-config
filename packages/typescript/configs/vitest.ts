1dc813ccd57a1e33d6758183d551d195bcbc7485
William Thorsen <william@thorsen.dev>
2025-04-29 00:43:37 -0700

    ts|feat: Add typechecking to Vitest config

--- packages/typescript/configs/vitest.ts
+++ packages/typescript/configs/vitest.ts
@@ -1,9 +1,25 @@
 import type { Linter } from 'eslint';
+import tseslint, { type ConfigArray } from 'typescript-eslint';
 
-async function createConfig(): Promise<Linter.Config> {
+// Modifications of rules that are not in the "recommended" config.
+const modifiedStrictRules: Linter.RulesRecord = {
+  'vitest/prefer-expect-assertions': 'off', // default: 'error'
+};
+
+async function createConfig(): Promise<ConfigArray> {
   const { default: vitestPlugin } = await import('@vitest/eslint-plugin');
 
-  return vitestPlugin.configs.all;
+  return tseslint.config({
+    extends: [vitestPlugin.configs.all],
+    rules: {
+      ...modifiedStrictRules,
+    },
+    settings: {
+      vitest: {
+        typecheck: true,
+      },
+    },
+  });
 }
 
 export default createConfig;
