0dd58ff052b468849cf71153c8f31933ff28ef70
William Thorsen <william@thorsen.dev>
2025-04-29 23:36:47 -0700

    ts|feat: Use full n configuration instead of rules only

--- packages/typescript/configs/n.ts
+++ packages/typescript/configs/n.ts
@@ -1,24 +1,19 @@
 import type { Linter } from 'eslint';
 import nPlugin from 'eslint-plugin-n';
+import tseslint from 'typescript-eslint';
 
-const rules: Linter.RulesRecord = {
-  'n/no-callback-literal': 'off',
+const modifiedRules: Linter.RulesRecord = {
+  'n/no-callback-literal': 'off', // Enforces Node.js-style callback pattern
   'n/no-extraneous-import': 'warn',
-  'n/no-missing-import': 'off',
-  'n/no-unpublished-import': 'off',
+  'n/no-missing-import': 'warn',
+  'n/no-unpublished-import': 'warn',
   // TODO: Enable this rule only in Node.js environments. See TOOLCHAIN-5.
   'n/no-unsupported-features/node-builtins': 'off',
 };
 
-const config = {
-  ...nPlugin.configs['flat/recommended-module'],
-  plugins: {
-    n: nPlugin,
-  },
-  rules: {
-    ...nPlugin.configs['flat/recommended-module'].rules,
-    ...rules,
-  },
-};
+const config = tseslint.config({
+  extends: [nPlugin.configs['flat/recommended']],
+  rules: modifiedRules,
+});
 
 export default config;
