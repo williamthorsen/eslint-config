2d874f254d3ac4a98be0e0f29a7e5de4cd95ebb6
William Thorsen <william@thorsen.dev>
2026-03-10 22:46:16 -0700

    strict-lint|feat: Prevent escalation of downgraded linting rules

--- packages/strict-lint/src/defaultMaxSeverity.ts
+++ packages/strict-lint/src/defaultMaxSeverity.ts
@@ -2,4 +2,8 @@ import type { MaxSeverityMap } from './types.ts';
 
 export const defaultMaxSeverity = {
   '@typescript-eslint/no-deprecated': 'warn',
+  'unicorn/consistent-function-scoping': 'warn',
+  'unicorn/no-useless-undefined': 'warn',
+  'unicorn/prefer-global-this': 'warn',
+  'unicorn/prefer-ternary': 'warn',
 } satisfies MaxSeverityMap;
